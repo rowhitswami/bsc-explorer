271635442715714553eb40cab20acb378d6aece8
Rohit Swami <rowhitswami1@gmail.com>
2020-08-01 23:51:44 +0530

    chore: modular

--- app/static/js/overview.js
+++ app/static/js/overview.js
@@ -1,11 +1,10 @@
-const BOUNDARIES = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/bangalore_boundaries.geojson?token=AD6AJSA4YZLSIT42BDBTMTC7FWAWQ'
-const BUS_STOPS = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/bus_stops.geojson?token=AD6AJSDCDSGUDHDB2QQP7S27FWGKM'
-const ROUTES = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/routes.json?token=AD6AJSBGLIIC2N6K6IRXJF27FWHJC'
-const SCHOOLS = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/schools.json?token=AD6AJSF3P3XKWQACUX6CP2K7FWHLA'
+var BOUNDARIES = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/bangalore_boundaries.geojson?token=AD6AJSA4YZLSIT42BDBTMTC7FWAWQ'
+var BUS_STOPS = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/bus_stops.geojson?token=AD6AJSDCDSGUDHDB2QQP7S27FWGKM'
+var ROUTES = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/routes.json?token=AD6AJSBGLIIC2N6K6IRXJF27FWHJC'
+var SCHOOLS = 'https://raw.githubusercontent.com/rowhitswami/bsc-explorer/master/app/data/schools.json?token=AD6AJSF3P3XKWQACUX6CP2K7FWHLA'
 
-var mymap = L.map('base-map', {
-    renderer: L.canvas()
-}).setView([12.9716, 77.59465], 11);
+
+var overview_map = L.map('overview-map', {fullscreenControl: true}).setView([12.9716, 77.59465], 10);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,7 +13,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoicm93aGl0c3dhbWkiLCJhIjoiY2tkN3A1dnRuMGJycjJ5czhqNmdscmZ1MyJ9.abR9R7t9yTNUfM3RON28AA'
-}).addTo(mymap);
+}).addTo(overview_map);
 
 var myStyle = {
     "color": "#00eaff",
@@ -22,9 +21,9 @@ var myStyle = {
     "opacity": 0.1
 };
 
-// $.getJSON(BOUNDARIES, function (data) {
-//     L.geoJson(data, { style: myStyle }).addTo(mymap);
-// });
+$.getJSON(BOUNDARIES, function (data) {
+    L.geoJson(data, { style: myStyle }).addTo(overview_map);
+});
 
 function busStopTooltip(feature, layer) {
     // does this feature have a property named popupContent?
@@ -49,7 +48,7 @@ $.getJSON(BUS_STOPS, function (data) {
             return L.circleMarker(latlng, bus_stop_style);
         },
         onEachFeature: busStopTooltip
-    }).addTo(mymap);
+    }).addTo(overview_map);
 });
 
 
@@ -85,10 +84,10 @@ $.getJSON(SCHOOLS, function (data) {
         }
     })
 
-    $.each(school_points, function(index, point) {
-        var tooltip  = point.name + ", " + point.address
-        tooltip += "(" + point.type + ")"
-        L.circleMarker([point['lat'], point['lng']], school_style).addTo(mymap).bindPopup(tooltip);
+    $.each(school_points, function (index, point) {
+        var tooltip = point.name + ", " + point.address
+        tooltip += " (" + point.type + ")"
+        L.circleMarker([point['lat'], point['lng']], school_style).addTo(overview_map).bindPopup(tooltip);
     })
 })
 
@@ -115,21 +114,21 @@ $.getJSON(ROUTES, function (data) {
         "reverse": false,
         "hardwareAccelerated": true
     });
-    mymap.addLayer(path);
-    mymap.fitBounds(path.getBounds())
+    overview_map.addLayer(path);
+    overview_map.fitBounds(path.getBounds())
 })
 
 
 /*Legend specific*/
 var legend = L.control({ position: "bottomleft" });
 
-legend.onAdd = function(map) {
-  var div = L.DomUtil.create("div", "legend");
-  div.innerHTML += '<i style="background: #00aeff"></i><span>Schools</span><br>';
-  div.innerHTML += '<i style="background: #ff0055"></i><span>Bus Stops</span><br>';
-  div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Bus Routes</span><br>';
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += '<i style="background: #00aeff"></i><span>Schools</span><br>';
+    div.innerHTML += '<i style="background: #ff0055"></i><span>Bus Stops</span><br>';
+    div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Bus Routes</span><br>';
 
-  return div;
+    return div;
 };
 
-legend.addTo(mymap);
+legend.addTo(overview_map);
